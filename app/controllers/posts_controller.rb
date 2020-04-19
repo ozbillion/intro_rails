825630cdd22202f5c4975e009b78149595a97c18
ozbillion <sky228822sky@gmail.com>
2020-04-19 16:03:52 +0900

    投稿一覧の表示順を新しいものが上に来るようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
   def show
     @post=Post.find_by(id:params[:id])
